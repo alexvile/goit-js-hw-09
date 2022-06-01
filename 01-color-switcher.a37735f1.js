bafc6e821b9b712b2b2222276d4284bd5a52fedb
alexvile <alexei20293@yahoo.com>
2022-06-01 09:15:22 +0000

    Deploying to gh-pages from @ alexvile/goit-js-hw-09@4bbcb93ecd09fe4f43bc274c982c1bb4b9e38b19 🚀

--- 01-color-switcher.a37735f1.js
+++
@@ -1,2 +0,0 @@
-var startBtn=document.querySelector("[data-start"),stopBtn=document.querySelector("[data-stop"),body=document.body;function getRandomHexColor(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}function onStartBtnClick(){body.style.backgroundColor=getRandomHexColor()}startBtn.addEventListener("click",onStartBtnClick);
-//# sourceMappingURL=01-color-switcher.a37735f1.js.map
