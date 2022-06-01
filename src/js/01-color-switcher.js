4bbcb93ecd09fe4f43bc274c982c1bb4b9e38b19
Alexei Solotvinskiy <alexei20293@yahoo.com>
2022-06-01 12:14:26 +0300

    solved 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,37 @@
-const startBtn = document.querySelector("[data-start");
-const stopBtn = document.querySelector("[data-stop");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
 const body = document.body;
 
 
-
-  startBtn.addEventListener('click', onStartBtnClick);
-
-
-//   const timerId = setInterval(callback, delay, arg1, arg2, ...);
-
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+let colorChangeInterval = null;
+let isActiveInterval = false;
 
 
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-  function onStartBtnClick() {
-    body.style.backgroundColor = getRandomHexColor();
-  }
+function paintBody() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartBtnClick() {
+  if (isActiveInterval) {
+    return
+  };
+
+  isActiveInterval = true;
+  colorChangeInterval = setInterval(paintBody, 1000);
+}
+
+function onStopBtnClick() {
+  isActiveInterval = false;
+  clearInterval(colorChangeInterval);
+}
+
+
+
+ 
+
