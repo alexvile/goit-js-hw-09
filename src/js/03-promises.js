b7a5c78d0dd4f907ecb7b52e0c3f78160ad63b69
Alexei Solotvinskiy <alexei20293@yahoo.com>
2022-06-03 15:17:11 +0300

    solved 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,248 +1,78 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function runPromise(position) {
-  
-  return new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.1;
-  const delay = 200
-  
-  setTimeout(() => {
-      if(shouldResolve) {
-        resolve({position, delay});
-      } else {
-        reject({position, delay});
-      }
-      
-    }, delay);
 
-});
+
+const firstDelayInput = document.querySelector('[name=delay]');
+const stepInput = document.querySelector('[name=step]');
+const amountInput = document.querySelector('[name=amount]');
+const formEl = document.querySelector('form');
+const sumbmitBtn = document.querySelector('[type=submit]');
+
+
+let firstDelayValue = 0;
+let stepValue = 0;
+let amountValue = 0;
+
+
+
+formEl.addEventListener('input', onFormInput);
+
+function onFormInput(e) {
+  firstDelayValue = firstDelayInput.value;
+  stepValue = stepInput.value;
+  amountValue = amountInput.value;
 }
 
-// runPromise('2', '200')
-// .then(({ position, delay }) => {
-//       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-//    })
-// .catch(({ position, delay }) => {
-//       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//     });
+sumbmitBtn.addEventListener('click', onSubmitClick);
 
-    // runPromise('2')
-    // .then(({ position, delay }) => {
-    //       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-    //    })
-    // .catch(({ position, delay }) => {
-    //       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    //     });
-
-
-const horses = [
-  '1',
-  '2',
-  '3'
-];
-
-const promises = horses.map(runPromise);
-
-console.log(promises);
-
-
-Promise.all(promises)
-.then( y  => {console.log(y)})
-.catch(x => {console.log(x)})
+function onSubmitClick(e) {
+  e.preventDefault();
+  callCreatePromises(firstDelayValue, stepValue, amountValue);
+}
 
 
 
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
 
-// .then(({ position, delay }) => {console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)})
-// .catch(({ position, delay }) => {
-//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
+  return new Promise((resolve, reject) => {
 
-// .then(({ position, delay }) => {
-//   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-// })
-// .catch(({ position, delay }) => {
-//   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-// });
+    if(shouldResolve){
+      resolve(`Fulfilled promise ${position} in ${Date.now() - delay}ms`)
+    }
+    else {
+      reject(`Rejected promise ${position} in ${Date.now() - delay}ms`)
+    }
+  
+  });
+};
 
-// runPromise([0])
-// .then(({ position, delay }) => {
-//   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-// })
-// .catch(({ position, delay }) => {
-//   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-// });
 
-// runPromise([1])
-// .then(({ position, delay }) => {
-//   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-// })
-// .catch(({ position, delay }) => {
-//   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-// });
+
+function callCreatePromises(firstDelay, step, amount) {
+    let counter = 0
+
+  const timeBefore = Date.now()
+
+
+    setTimeout(() => {
+
+        const timerId2 = setInterval(() => {
+          counter += 1;
+          if(counter > amount) {
+            clearInterval(timerId2);
+            return
+          }
+          createPromise(counter, timeBefore)
+          .then(resolve => {  Notify.success(resolve); console.log(resolve)} )
+          .catch(error => { Notify.failure(error); console.log(error)});
     
+      }, step)
 
-
-// / createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-// function onFullfilled(result) {
-//   console.log(`+ ${result}`);
-// }
-// function onRejected(error) {
-//   console.log(`- ${error}`);
-// }
-
-
-// console.log(horses);
-
-// const promises = horses.map(createPromise);
-// // console.log(promises);
-
-// console.log(Promise.all(promises))
-
-
-
-  // }, )
-
-
-
-  // if (shouldResolve) {
-  //   // Fulfill
-  // } else {
-  //   // Reject
-  // }
-// }
+    
+    }, firstDelay)
+}
 
 
 
 
-
-
-
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-
-
-// function run(name) {
-//   return new Promise((resolve) => {
-//     const time = getRandomTime(1000, 1500)
-
-//     setTimeout(() => {
-//         resolve({name, time})
-//     }, time)
-//   })
-// }
-
-// run('qwe')
-// .then(x => console.log(x))
-// .catch(e => console.log(e))
-
-
-// function getRandomTime(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-  
-// }
-
-
-
-
-
-
-
-
-// const promise = new Promise((resolve, reject) => {
-//   const canFullfilled = Math.random() > 0.5;
-
-//   setTimeout(() => {
-//       if(canFullfilled) {
-//         resolve('can resolve');
-//       }
-//       reject('error');
-  
-//     }, 1000)
-// })
-
-// // promise.then(
-// //   result => {
-// //   console.log(`+ ${result}`);
-// // },
-// // error => {
-// //   console.log(`- ${error}`);
-// // })
-
-// // promise.then(onFullfilled, onRejected);
-
-// function onFullfilled(result) {
-//   console.log(`+ ${result}`);
-// }
-// function onRejected(error) {
-//   console.log(`- ${error}`);
-// }
-
-
-
-// promise
-//   .then(onFullfilled)
-//   .then(x => {
-//     console.log(x);
-//     return 10;
-//   })
-//   .then(y => {
-//     console.log(y);
-//   })
-// //   .catch(error => console.log(error))
-// //   .finally(() => console.log('finally'));
-
-// const makeOrder = dish => {
-//   const DELAY = 1000;
-
-//   return new Promise((resolve, reject) => {
-//      const passed = Math.random() > 0.5;
-
-
-//      setTimeout(() => {
-//        if(passed) {
-//          resolve(`vot bludo ${dish}`);
-//        }
-
-//        reject('nema bluda');
-//      }, DELAY);
-//   });
-//   // return promise;
-// };
-
-
-
-// // const p = makeOrder('pirozhok');
-// makeOrder('pirozhok').then(onSuccess).catch(onError);
-
-// function onSuccess(result) {
-//   console.log(result);
-// };
-// function onError(error) {
-//   console.log(error);
-// }
